8633a47eb42ed044c6e183a955f435b9f9b10848
daniel.silva010379@gmail.com <heloisaribeiro008@gmail.com>
2022-08-30 09:37:25 -0300

    Inserido buscar elemento

--- ListaLinear/ListaLinear/ListaLinear.cpp
+++ ListaLinear/ListaLinear/ListaLinear.cpp
@@ -103,5 +103,25 @@ void inserirElemento()
 
 void buscarElemento()
 {
+	int pos = -1;
+	int busca = 0;
 
+	cout << "Digite o elemento desejado: \n";
+	cin >> busca;
+
+	//backup cout << "O valor e': " << busca << endl;
+
+	for (int n = 0; n < nElementos; n++)
+	{
+		if (busca == lista[n]) {
+
+			cout << "Encontrado pos: " << n << endl;
+			pos = n;
+		}
+	}
+
+	if (pos == -1)
+	{
+		cout << "Nao encontrado\n" << endl;
+	}
 }
